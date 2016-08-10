382c7c08857c2b70e56b5fdd5032d6ef378bbfc0
Mihai Paraschiv <mihai.paraschiv@gmail.com>
2016-08-10 14:30:04 +0300

    Version 0.2.1.

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,12 @@
 # Copyright (c) Mihai Paraschiv.
 # Distributed under the terms of the Modified BSD License.
 
-GIT_MASTER_HEAD_SHA=$(git rev-parse --short=12 --verify HEAD)
 OWNER=mihaiparaschiv
+VERSION=0.2.1
 
 
 docker build -t ${OWNER}/data-science-base:latest ./data-science-base
-docker build -t ${OWNER}/data-science-base:${GIT_MASTER_HEAD_SHA} ./data-science-base
+docker build -t ${OWNER}/data-science-base:${VERSION} ./data-science-base
 
 docker build -t ${OWNER}/data-science-spark:latest ./data-science-spark
-docker build -t ${OWNER}/data-science-spark:${GIT_MASTER_HEAD_SHA} ./data-science-spark
+docker build -t ${OWNER}/data-science-spark:${VERSION} ./data-science-spark
