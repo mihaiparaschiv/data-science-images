ec7cbc1779026e0b82753f2f383157417274b551
Mihai Paraschiv <mihai.paraschiv@gmail.com>
2016-08-10 14:26:45 +0300

    Added latest tags.

--- build-images.sh
+++ build-images.sh
@@ -5,4 +5,8 @@ GIT_MASTER_HEAD_SHA=$(git rev-parse --short=12 --verify HEAD)
 OWNER=mihaiparaschiv
 
 
+docker build -t ${OWNER}/data-science-base:latest ./data-science-base
 docker build -t ${OWNER}/data-science-base:${GIT_MASTER_HEAD_SHA} ./data-science-base
+
+docker build -t ${OWNER}/data-science-spark:latest ./data-science-spark
+docker build -t ${OWNER}/data-science-spark:${GIT_MASTER_HEAD_SHA} ./data-science-spark
